3f0dfe8f2a806fa226101180801d0e24ec1e2c17
Dan Bickford <danbickford007@yahoo.com>
2013-07-07 14:54:05 -0400

    xxx

--- lib/redmine/export/pdf.rb
+++ lib/redmine/export/pdf.rb
@@ -17,7 +17,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-require 'iconv'
+#require 'iconv'
 require 'tcpdf'
 require 'fpdf/chinese'
 require 'fpdf/japanese'
